adf7e39536c46d7a50847e121545b3d59908a1a1
kth <spmm528@gmail.com>
2022-07-26 16:00:23 +0900

    a

--- src/com/bnt/study/ch02/Hello.java
+++ src/com/bnt/study/ch02/Hello.java
@@ -1,22 +1,28 @@
 package com.bnt.study.ch02;
 
-import java.sql.Date;
-
 public class Hello {
 
 	public static void main(String[] args) {
 
 //		데이터 타입 변수명 = 값;
 //		데이터 타입 변수명
-
-		boolean abc = true;
-		boolean xyz;
-		xyz = false;
+		int width = 100;
+		String name = "김태희";
+//		상세화면
+		String title = "트랜스포머 범블비 옵티머스 로봇변신 무선조종 RC카";
+		int price = 41500;
+		boolean overSea = true;
+		int discount = 0;
+		int discointType = 0;
+		int totalPrice = 0;
+		int option = 0;
+		boolean like = false;
+		String link = "www.sdlkfnsl.com/fdahb";
+		String orderLink = "www.fdokngvfdl.com/fdagdf";
+		String basketLink = "www.fdndlfg.com/sdfs";
+		String imgLink = "www.dfogn.com/sfdgvdf";
 		
-		String a = "abcdefg";
-		Date b = "2022";
 		
 		
 	}
-
 }
