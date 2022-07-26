4f7070de67eb120cba8bc269e71fd1c4c8967b4f
kth <spmm528@gmail.com>
2022-07-26 15:37:53 +0900

    aa

---
+++ src/com/bnt/study/ch02/Hello.java
@@ -0,0 +1,22 @@
+package com.bnt.study.ch02;
+
+import java.sql.Date;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+//		데이터 타입 변수명 = 값;
+//		데이터 타입 변수명
+
+		boolean abc = true;
+		boolean xyz;
+		xyz = false;
+		
+		String a = "abcdefg";
+		Date b = "2022";
+		
+		
+	}
+
+}
