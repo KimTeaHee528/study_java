26617e5147ba4b1a6fd44a4bc5d689aa25ff9110
kth <spmm528@gmail.com>
2022-08-01 17:31:04 +0900

    a

---
+++ src/com/bnt/study/ch02/CastingExample.java
@@ -0,0 +1,51 @@
+package com.bnt.study.ch02;
+
+public class CastingExample {
+
+	public static void main(String[] args) {
+
+		double doubleValue = 1234567;
+		System.out.println("doubleValue : " + doubleValue);
+		
+		double doubleValue2 = 1234567891;
+		System.out.println("doubleValue2 : " + doubleValue2);
+
+		System.out.println("=========double을 float에 넣을때========");
+
+		float floatValue = (float)doubleValue;
+		System.out.println("floatValue : " + floatValue);
+		float floatValue2 = (float)doubleValue2;
+		System.out.println("floatValue : " + floatValue2 + " 오차생김\n");
+		
+
+		System.out.println("=========float을 더 작은데 넣을때========");
+		long longValue = (long)floatValue;
+		System.out.println("longValue : " + longValue);
+		
+		int intValue = (int)longValue;
+		System.out.println("intValue : " + intValue);
+		
+		short shortValue = (short)intValue;
+		System.out.println("shortValue : " + shortValue);
+		
+		byte byteValue = (byte)shortValue;
+		System.out.println("byteValue : " + byteValue);
+		
+		
+		
+		System.out.println("=========int를 string으로 변환========");
+		int intValue2 = 100;
+		System.out.println("intValue2 : " + intValue2);
+		
+		String stringValue = String.valueOf(intValue2);
+		System.out.println("stringValue : " + stringValue);
+		
+
+		System.out.println("=========string을 int로 변환========");
+		String stringValue2 = "756148";
+		System.out.println("stringValue2 : " + stringValue2);
+		int intValue3 = Integer.parseInt(stringValue2);
+		System.out.println("intValue3 : " + intValue3);
+	}
+
+}
--- src/com/bnt/study/ch02/PromotionExample.java
+++ src/com/bnt/study/ch02/PromotionExample.java
@@ -12,26 +12,42 @@ public class PromotionExample {
 		System.out.println("byteValue : " + byteValue);
 		
 		int intValue = byteValue;
-		System.out.println("intValue : " + intValue);
+		System.out.println("바이트를 인트로 : " + intValue);
 
 		int intValue2 = 2134567890;
 		System.out.println("intValue2 : " + intValue2);
 
 		byte byteValue2 = (byte) intValue;
-		System.out.println("bytetValue2 : " + byteValue2);
+		System.out.println("인트를 바이트로 : " + byteValue2);
 		
 		double doubleValue = 21345.67890;
 		System.out.println("intValue2 : " + intValue2);
 
 		int intValue3 = (int) doubleValue;
-		System.out.println("intValue3 : " + intValue3);
-		
+		System.out.println("더블을 인트로 : " + intValue3);
 		
 		int intValue4 = 200;
 		System.out.println("intValue4 : " + intValue4);
 		
 		double doubleValue2 = intValue4;
-		System.out.println("doubleValue2 : " + doubleValue2);
+		System.out.println("인트를 더블로 : " + doubleValue2);
+		
+		System.out.println("==========형변환==========");
+		
+		byte byteValue3 = 10;
+		System.out.println("바이트 : " + byteValue3);
+		short shortvalue = byteValue3;
+		System.out.println("byte를 short에 넣으면 : " + shortvalue);
+
+		int intValue10 = shortvalue;
+		System.out.println("short을 int에 넣으면 : " + intValue10);
+		long longValue10 = intValue10;
+		System.out.println("그 int를 long에 넣으면 : " + longValue10);
+		float floatValue = longValue10;
+		System.out.println("그 long을 float에 넣으면 : " + floatValue);
+		double doubleValue10 = floatValue;
+		System.out.println("그 float을 double에 넣으면 : " + doubleValue10);
+
 		
 		
 	}
