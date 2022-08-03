85e16584f8873f16653a1ea01242686ebb3e8ca3
kth <spmm528@gmail.com>
2022-08-03 17:24:32 +0900

    a

--- src/com/bnt/study/ch03/AritheticOperatorExample.java
+++ src/com/bnt/study/ch03/AritheticOperatorExample.java
@@ -27,5 +27,42 @@ public class AritheticOperatorExample {
 		double i = 1;
 		double d2 = d%i;
 		System.out.println(d2);
+		
+//===========================================
+
+		System.out.println("===========================================");
+		double resultA1 = (1+((double)2/3)-((double)5/8))*(9+((double)4/5));
+		System.out.println(resultA1);
+		
+		double resultA2 = 5-(3.25-(1+(double)5/6))*(double)12/17;
+		System.out.println(resultA2);
+		
+		System.out.println("===========================================");
+		sum(8,7,5);
+		sum(1,9,5);
+		sum(5,6,6);
+		sum(4,7,1);
+		sum(7,5,5);
+
+		System.out.println("===========================================");
+		average("홍길동",60,70,85,90,45);
+		average("박말순",50,75,70,60,70);
+		average("고장난",55,60,64,58,90);
+		average("도롱뇽",80,70,63,88,78);
+		
+		
+		
+		
+
 	}
+	public static void sum(int a,int b,int c) {
+		System.out.println(a + "+" +b + "+" + c + " = " + (a+b+c));
+	}
+
+	public static void average(String name, int engleish, int math, int physics, int music, int athletic){
+		System.out.println(name+ "의 평균점수는 " + (double) (engleish + math + physics + music + athletic)/5);
+	}
+	
+	
+	
 }
---
+++ src/com/bnt/study/ch03/CompareOperatorExample.java
@@ -0,0 +1,31 @@
+package com.bnt.study.ch03;
+
+public class CompareOperatorExample {
+
+	public static void main(String[] args) {
+
+		int num1 = 10;
+		int num2 = 20;
+
+		boolean result1 = (num1 == num2);
+		System.out.println(result1);
+
+		boolean result2 = (num1 != num2);
+		System.out.println(result2);
+
+		boolean result3 = (num1 < num2);
+		System.out.println(result3);
+
+		boolean result4 = (num1 <= num2);
+		System.out.println(result4);
+
+		boolean result5 = (num1 > num2);
+		System.out.println(result5);
+
+		boolean result6 = (num1 >= num2);
+		System.out.println(result6);
+
+		
+	}
+
+}
