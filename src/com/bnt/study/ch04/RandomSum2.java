33ba877861a45a9b54090ff1cb30e942669d8d46
kth <spmm528@gmail.com>
2022-08-16 14:45:14 +0900

    a

--- src/com/bnt/study/ch04/RandomSum2.java
+++ src/com/bnt/study/ch04/RandomSum2.java
@@ -18,13 +18,14 @@ public class RandomSum2 {
 			minA = random1;
 		}
 		//최소값부터 최대값까지 더하기
-		for(int i = minA ; i<=maxA ;i++) {
+		for(int i = minA+1 ; i<maxA ;i++) {
 			result +=i;
 		}
-		System.out.println(minA + "부터 " + maxA + "까지의 합은 : " + result);
+		System.out.println("랜덤값" + minA +", "+maxA);
+		System.out.println((minA+1) + "부터 " + (maxA-1) + "까지의 합은 : " + result);
 		
 		
-		int resultA = (maxA+minA)*(maxA-minA+1)/2;
+		int resultA = (maxA+minA)*(maxA-minA-1)/2;
 		System.out.println("확인 \n" + resultA);
 	}
 }
