d3c40c7f47bf7474b9511b239b61dc80b1acf044
kth <spmm528@gmail.com>
2022-08-19 17:30:35 +0900

    a

--- src/com/bnt/study/ch05/LottoPlease2.java
+++ src/com/bnt/study/ch05/LottoPlease2.java
@@ -3,7 +3,7 @@ package com.bnt.study.ch05;
 import java.util.Arrays;
 
 public class LottoPlease2 {
-
+ 
 	public static void main(String[] args) {
         String[] strArr = {
     		"[5, 7, 12, 13, 18, 35]",
