f853d965e2169b6c2c39b26738ed420a583990fb
kth <spmm528@gmail.com>
2022-08-24 17:08:12 +0900

    a

---
+++ src/com/bnt/study/ch06/Television.java
@@ -0,0 +1,11 @@
+package com.bnt.study.ch06;
+
+public class Television {
+	public static String company = "Samsung";
+	public static String model = "OLED";
+	public static String info;
+	public String version = "1.0";
+	static {
+		info = company +" "+model;
+	}
+}
