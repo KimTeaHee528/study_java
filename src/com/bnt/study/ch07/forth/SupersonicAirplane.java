24c1e57876b394bf3d600b512c6ee3b14685b193
kth528 <spmm528@gmail.com>
2022-08-26 17:16:42 +0900

    aa

--- src/com/bnt/study/ch07/forth/SupersonicAirplane.java
+++ src/com/bnt/study/ch07/forth/SupersonicAirplane.java
@@ -1,15 +1,13 @@
 package com.bnt.study.ch07.forth;
 
 public class SupersonicAirplane extends Airplane {
-	public static final int normal = 1;
-	public static final int supersonic = 2;
-	public int flyMode = normal;
+	public static final int NORMAL = 1;
+	public static final int SUPERSONIC = 2;
+	public int flyMode = NORMAL;
+
 	@Override
 	public void fly() {
-		if(flyMode == supersonic) {
-			System.out.println("초음속비행합니다.");
-		}else {
-			super.fly();
-		}
+		if (flyMode == SUPERSONIC) {System.out.println("초음속비행");}
+		else {super.fly();}
 	}
 }
--- src/com/bnt/study/ch07/forth/SupersonicAirplaneExample.java
+++ src/com/bnt/study/ch07/forth/SupersonicAirplaneExample.java
@@ -4,8 +4,22 @@ public class SupersonicAirplaneExample {
 
 	public static void main(String[] args) {
 		SupersonicAirplane supersonicAirplane = new SupersonicAirplane();
+		supersonicAirplane.takeoff();
+		supersonicAirplane.fly();
+		supersonicAirplane.flyMode = SupersonicAirplane.SUPERSONIC;
+		supersonicAirplane.fly();
+		supersonicAirplane.flyMode = SupersonicAirplane.NORMAL;
 		supersonicAirplane.fly();
 		supersonicAirplane.land();
-		supersonicAirplane.takeoff();
+		
+		System.out.println("===============");
+		SupersonicAirplane supersonicAirplane2 = new SupersonicAirplane();
+		supersonicAirplane2.takeoff();
+		supersonicAirplane2.fly();
+		supersonicAirplane2.flyMode = Constants.SUPERSONIC;
+		supersonicAirplane2.fly();
+		supersonicAirplane2.flyMode = Constants.NORMAL;
+		supersonicAirplane2.fly();
+		supersonicAirplane2.land();
 	}
 }
