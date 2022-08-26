4ecaa7a0f69b591c74f9f845417f86fbd8f82cbb
kth528 <spmm528@gmail.com>
2022-08-26 16:07:58 +0900

    a

--- src/com/bnt/study/ch07/second/Student.java
+++ src/com/bnt/study/ch07/second/Student.java
@@ -1,17 +1,13 @@
 package com.bnt.study.ch07.second;
 
 public class Student extends People {
+	
 	public int studentNo;
 
-	public Student() {
-		super();
-	}
-	public Student(String name, String ssn) {
-		super(name, ssn);
-	}
+	public Student() {super();}
+	public Student(String name, String ssn) {super(name, ssn);}
 	public Student(String name, String ssn, int studentNo) {
 		super(name, ssn);
 		this.studentNo = studentNo;
 	}
 }
- 
--- src/com/bnt/study/ch07/second/StudentExample.java
+++ src/com/bnt/study/ch07/second/StudentExample.java
@@ -3,15 +3,16 @@ package com.bnt.study.ch07.second;
 public class StudentExample {
 
 	public static void main(String[] args) {
-		Student student1 = new Student("김태희1","ssn1");
-		Student student2 = new Student("김태희2","ssn2",5);
-		
+		Student student1 = new Student("김태희1", "ssn1");
+
 		System.out.println("=======student1-부모=======");
 		System.out.println(student1.name);
 		System.out.println(student1.ssn);
 		System.out.println("=======student1-자식=======");
 		System.out.println(student1.studentNo);
-		
+
+		Student student2 = new Student("김태희2", "ssn2", 5);
+
 		System.out.println("=======student2-부모=======");
 		System.out.println(student2.name);
 		System.out.println(student2.ssn);
